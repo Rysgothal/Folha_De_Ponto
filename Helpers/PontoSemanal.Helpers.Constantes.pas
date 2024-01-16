84303c6e14f648075dfdf8d7dead6d6ca59320be
Rysgothal <souzafrade2014@gmail.com>
2024-01-16 10:43:35 -0400

    Atualizando Regex

--- Helpers/PontoSemanal.Helpers.Constantes.pas
+++ Helpers/PontoSemanal.Helpers.Constantes.pas
@@ -5,17 +5,36 @@ interface
 type
   TConstantes = class abstract
   public
-    const REGEX_CODIGO_FUNCIONARIO = '(Código|Codigo)\W{0,}(\d{0,7})';
-    const REGEX_NOME_FUNCIONARIO = '(Funcionário|Funcionario)\W{0,}(.*)\R';
-    const REGEX_ADMISSAO_FUNCIONARIO = '(Admissão|Admissao)\W{0,}(\d{2}[\/]\d{2}[\/]\d{4})';
+    const REGEX_NAO_PALAVRAS = '\W{0,}?';
+    const REGEX_NAO_DIGITOS = '\D{0,}?';
+    const REGEX_HORARIO_VALIDO = '([0,1][0-9]:[0-5][0-9]|[2][0-3]:[0-5][0-9])';
+    const REGEX_SEGUNDA = 'Segunda';
+    const REGEX_TERCA = 'Ter[c|ç]a';
+    const REGEX_QUARTA = 'Quarta';
+    const REGEX_QUINTA = 'Quinta';
+    const REGEX_SEXTA = 'Sexta';
+    const REGEX_SABADO = 'S[a|á]bado';
+
+    const REGEX_CODIGO_FUNCIONARIO = '(C[ó|o]digo)\W{0,}(\d{0,7})';
+    const REGEX_NOME_FUNCIONARIO = '(Funcion[á|o]nario)\W{0,}(.*)\R';
+    const REGEX_ADMISSAO_FUNCIONARIO = '(Admiss[ã|a]o)\W{0,}(\d{2}[\/]\d{2}[\/]\d{4})';
     const REGEX_JORNADA_FUNCIONARIO = '(Jornada\sSemanal)\W{0,}(\d{0,3})';
     const REGEX_INTERVALO_FUNCIONARIO = '(Intervalo\s?Almo[ç|c]o)\W{0,}(\d{2}[\:]\d{2})';
     const HASH_SALTING = '3175634@0733b$66ç723464335&d5d5*b5d5ód~2d5b2d3d5b';
 
-    const REGEX_HORARIOS = '(Segunda|Terça|Quarta|Sexta|Quinta)\D{0,}?\W{0,}?([0,1][0-9]:[0-5][0-9]|[2][0-3]:[0-5][0' +
-      '-9])\W{0,}?([0,1][0-9]:[0-5][0-9]|[2][0-3]:[0-5][0-9])\W{0,}?([0,1][0-9]:[0-5][0-9]|[2][0-3]:[0-5][0-9])\W{0,' +
-      '}?([0,1][0-9]:[0-5][0-9]|[2][0-3]:[0-5][0{0,}([0,1][0-9]:[0-5][0-9]|[2][0-3]:[0-5][0-9])|(Sábado|Sabado)\D{0,' +
-      '}?\W{0,}([0,1][0-9]:[0-5][0-9]|[2][0-3]:[0-5][0-9])\W{0,}([0,1][0-9]:[0-5][0-9]|[2][0-3]:[0-5][0-9])';
+    const REGEX_FOLHA_HORARIOS_DIA_UTIL = REGEX_NAO_DIGITOS + REGEX_NAO_PALAVRAS + REGEX_HORARIO_VALIDO +
+      REGEX_NAO_PALAVRAS + REGEX_HORARIO_VALIDO + REGEX_NAO_PALAVRAS + REGEX_HORARIO_VALIDO + REGEX_NAO_PALAVRAS +
+      REGEX_HORARIO_VALIDO;
+
+    const REGEX_FOLHA_HORARIOS_NAO_UTIL = REGEX_NAO_DIGITOS + REGEX_NAO_PALAVRAS + REGEX_HORARIO_VALIDO +
+      REGEX_NAO_PALAVRAS + REGEX_HORARIO_VALIDO;
+
+    const REGEX_SEGUNDA_HORARIOS = REGEX_SEGUNDA + REGEX_FOLHA_HORARIOS_DIA_UTIL;
+    const REGEX_TERCA_HORARIOS = REGEX_TERCA + REGEX_FOLHA_HORARIOS_DIA_UTIL;
+    const REGEX_QUARTA_HORARIOS = REGEX_QUARTA + REGEX_FOLHA_HORARIOS_DIA_UTIL;
+    const REGEX_QUINTA_HORARIOS = REGEX_QUINTA + REGEX_FOLHA_HORARIOS_DIA_UTIL;
+    const REGEX_SEXTA_HORARIOS = REGEX_SEXTA + REGEX_FOLHA_HORARIOS_DIA_UTIL;
+    const REGEX_SABADO_HORARIOS = REGEX_SABADO + REGEX_FOLHA_HORARIOS_NAO_UTIL;
     end;
 
 implementation
